4ebc5d732381e4581a022cf435a076bac29d824b
jimmy-is-me <76391627+jimmy-is-me@users.noreply.github.com>
2026-08-20 01:02:51 +0800

    fix(captcha): register standard WU Toolbox route

--- modules/captcha/module.php
+++ modules/captcha/module.php
@@ -517,7 +517,7 @@ add_action('admin_init', function() {
 
 add_action('admin_menu', function() {
 	add_submenu_page(
-		'wumetax-toolkit',
+		'wu-toolbox-modular',
 		'驗證碼設定',
 		'驗證碼設定',
 		'manage_options',
