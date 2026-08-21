585576f683da8fe82a80723ed5011e0fed1cbdbf
jimmy-is-me <76391627+jimmy-is-me@users.noreply.github.com>
2026-08-22 01:49:35 +0800

    Bump contact widgets module version

--- modules/contact-widgets/module.php
+++ modules/contact-widgets/module.php
@@ -5,7 +5,7 @@
  */
 defined('ABSPATH') || exit;
 
-define('WUTM_CONTACT_WIDGETS_VERSION', '1.2.3');
+define('WUTM_CONTACT_WIDGETS_VERSION', '1.2.4');
 define('WUTM_CONTACT_WIDGETS_PLUGIN_FILE', __FILE__);
 define('WUTM_CONTACT_WIDGETS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WUTM_CONTACT_WIDGETS_PLUGIN_URL', plugin_dir_url(__FILE__));
