d340c37e24c974a018d8f275654e549d67b8709f
jimmy-is-me <76391627+jimmy-is-me@users.noreply.github.com>
2026-08-23 18:09:15 +0800

    Add website URL update module source

---
+++ modules/go-live-update-urls/src/Updaters/Repo.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WUTM\GoLive\Updaters;
+
+use WUTM\GoLive\Traits\Singleton;
+
+/**
+ * Repository for the Updater classes.
+ *
+ * @author  OnPoint Plugins
+ * @since   6.0.0
+ */
+class Repo {
+	use Singleton;
+
+	/**
+	 * Get all registered updaters by classname.
+	 *
+	 * @return array<string, class-string<Updaters_Abstract>>
+	 */
+	public function get_updaters(): array {
+		$updaters = apply_filters( 'wutm-go-live-update-urls/updaters/repo/updaters', [
+			'url-encoded' => Url_Encoded::class,
+		] );
+		if ( ! \is_array( $updaters ) ) {
+			return [];
+		}
+
+		\uasort( $updaters,
+			/**
+			 * Sort the updater classes by priority.
+			 *
+			 * @param class-string<Updaters_Abstract> $a
+			 * @param class-string<Updaters_Abstract> $b
+			 */
+			function( $a, $b ) {
+				return $a::get_priority() <=> $b::get_priority();
+			}
+		);
+
+		return $updaters;
+	}
+}
