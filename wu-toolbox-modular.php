82bdf8036702a30980bd8979556868651365aeab
jimmy-is-me <76391627+jimmy-is-me@users.noreply.github.com>
2026-09-03 23:47:17 +0800

    chore: bump version to 1.7.5

--- wu-toolbox-modular.php
+++ wu-toolbox-modular.php
@@ -3,7 +3,7 @@
  * Plugin Name: WU Toolbox Modular
  * Plugin URI: https://wumetax.com/
  * Description: WU Toolbox 的按需載入模組化版本。每項功能獨立，只有啟用後才會載入。
- * Version: 1.7.4
+ * Version: 1.7.5
  * Author: WUMETAX
  * Author URI: https://wumetax.com/
  * License: GPL-2.0-or-later
@@ -14,7 +14,7 @@
 defined('ABSPATH') || exit;
 
 define('WUTM_FILE', __FILE__);
-define('WUTM_VERSION', '1.7.4');
+define('WUTM_VERSION', '1.7.5');
 define('WUTM_PATH', plugin_dir_path(__FILE__));
 define('WUTM_URL', plugin_dir_url(__FILE__));
 
